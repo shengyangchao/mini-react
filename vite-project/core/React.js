c5b8d98d84e3cb3240646b9502a05470b4c869ec
chaoshengyang <chaoshengyang126@126.com>
2024-01-14 21:30:01 +0800

    创建fiber

--- vite-project/core/React.js
+++ vite-project/core/React.js
@@ -18,20 +18,81 @@ const createElement = (type, props, ...children) => {
         },
     }
 }
+let nextWorkUnit = null
 const renderDom = (container, el) => {
-    const dom =
-        el.type === 'textNode' ? document.createTextNode('') : document.createElement(el.type)
-    Object.keys(el.props).forEach((key) => {
-        if (key !== 'children') {
-            dom[key] = el.props[key]
-        }
-    })
-    const children = el.props.children
-    children.forEach((child) => {
-        renderDom(dom, child)
-    })
-    container.append(dom)
+    nextWorkUnit = {
+        dom: container,
+        props: { children: [el] },
+    }
+    // const dom =
+    //     el.type === 'textNode' ? document.createTextNode('') : document.createElement(el.type)
+    // Object.keys(el.props).forEach((key) => {
+    //     if (key !== 'children') {
+    //         dom[key] = el.props[key]
+    //     }
+    // })
+    // const children = el.props.children
+    // children.forEach((child) => {
+    //     renderDom(dom, child)
+    // })
+    // container.append(dom)
 }
+
+const workLoop = (deadLine) => {
+    let showYield = false
+    while (!showYield && nextWorkUnit) {
+        //执行work,返回下一个任务
+        nextWorkUnit = performanceWorkUnit(nextWorkUnit)
+        showYield = deadLine.timeRemaining() < 1
+    }
+    requestIdleCallback(workLoop)
+}
+const performanceWorkUnit = (work) => {
+    //和renderDom操作一样，创建元素，append，处理props,处理children(将树转换成列表，边转换边append元素)
+    if (!work.dom) {
+        const dom = (work.dom =
+            work.type === 'textNode'
+                ? document.createTextNode('')
+                : document.createElement(work.type))
+        //插入
+        work.parent.dom.append(dom)
+        //处理props
+        Object.keys(work.props).forEach((key) => {
+            if (key !== 'children') {
+                dom[key] = work.props[key]
+            }
+        })
+    }
+    const children = work.props.children
+    let prevChild = null //记录上一个节点
+    children.forEach((child, index) => {
+        //为了不破坏虚拟dom的结构，重新创建fiber结构
+        const newFiber = {
+            type: child.type,
+            props: child.props,
+            dom: null,
+            parent: work,
+            child: null,
+            sibling: null,
+        }
+        if (index === 0) {
+            //添加子节点的指向
+            work.child = newFiber
+        } else {
+            prevChild.sibling = newFiber
+        }
+        prevChild = newFiber
+    })
+    //返回下一个任务（下一个节点），返回的优先级 孩子->兄弟->叔叔
+    if (work.child) {
+        return work.child
+    }
+    if (work.sibling) {
+        return work.sibling
+    }
+    return work.parent?.sibling
+}
+requestIdleCallback(workLoop)
 
 const React = {
     createElement,
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,3 @@
 import ReactDom from './core/ReactDom.js'
 import App from './App.jsx'
-console.log(123)
 ReactDom.createRoot(document.querySelector('#root')).render(App)
